8102b93f74a523bc55acc349f42a6dccf7ca492f
emilepetrin2-cell <emilepetrin2@gmail.com>
2026-09-15 21:16:26 -0400

    Refactor angle calculation using dot product

--- src/rules/angles.py
+++ src/rules/angles.py
@@ -1,12 +1,17 @@
-# src/rules/angles.py
 import numpy as np
-
 def calculer_angle(a, b, c):
     a = np.array(a)
     b = np.array(b)
     c = np.array(c)
-    radian = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
-    angle = np.abs(radian * 180.0 / np.pi)
-    if angle > 180.0:
-        angle = 360.0 - angle
+    v = a - b
+    u = c - b
+    scalaire = np.dot(v, u)
+    euclide1 = np.linalg.norm(u)
+    euclide2 = np.linalg.norm(v)
+    if euclide1 == 0 or euclide2 == 0:
+        return 0.0
+    cosinus = (scalaire / (euclide1 * euclide2))
+    angle_clip = np.clip(cosinus, -1, 1)
+    angle_arcos = np.arccos(angle_clip)
+    angle = float(np.degrees(angle_arcos))
     return angle
